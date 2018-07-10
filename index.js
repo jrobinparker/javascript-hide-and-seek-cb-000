bbe51a43ecd41c647085cf7cb6727a747f82cc8f
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 02:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function getFirstSelector(selector) {
-  const gotSelector = document.getElementsByTagName(selector)
+  const gotSelector = document.querySelector(selector)
 }
 
 function nestedTarget() {
-  const div = document.getElementsByTagName("div")[0]
-  const nested = div.children[1]
-  const target = target.getElementById("target")[0]
+  
 }
